dd581f5341145c577e08e2eb9bc9d7e5b585f793
karineunderwood <karineunderwood23@gmail.com>
2022-03-21 15:33:39 -0700

    Finished word_cout()function

--- wordcount.py
+++ wordcount.py
@@ -1,3 +1,49 @@
+#make a function
+#open a file, count how many times each space-separated word occurs in the file
+#return eaech word and the number of times it occurs
+#close the file
+
+def word_count(file):
+    file_to_open = open(file)
+
+    # for line in file_to_open:
+    #     line.rstrip()
+    #     line.split()
+    #     for char in line:
+    #         empty_str = ""
+    #         print(char)
+    #         if char.isalpha():
+    #             empty_str += char
+    #         elif char == " ":
+    #             empty_str += " "
+    #         else:
+    #             char = ""
+    #     print(empty_str)
+    
+    for line in file_to_open:
+        line = line.rstrip()
+        words = line.split()
+        how_many_words_dict = {}
+        for word in words:
+            # if not word[-1].isalpha():
+            #     word = word[:-1]
+            how_many_words_dict[word] = how_many_words_dict.get(word, 0) + 1
+            # else:
+            #     how_many_words_dict[word] = how_many_words_dict.get(word, 0) + 1
+
+
+
+        print(f'{how_many_words_dict}')
+
+            
+            # if char == char.isalpha():
+            #     char = ""
+            # print(char)
+                
+      
+
+
+word_count("test.txt")
 """Count words in file."""
 
 
