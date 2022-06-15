8fd82b212d6d00896244c5571648ff43445d202f
karineunderwood <karineunderwood23@gmail.com>
2022-06-15 10:59:20 -0700

    Dictionary done

--- wordcount.py
+++ wordcount.py
@@ -3,4 +3,27 @@
 
 # open the file
 # create an empty dictionary
+# split the words by empty space
+# iterate over each word and save into the dict
+
+data = open("test.txt")
+
+word_count = {}
+
+for line in data:
+    line = line.rstrip()
+
+    words = line.split()
+
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
+
+for word, count in word_count.items():
+    print(f"Word: {word}, count: {count}")
+
+
+
+
+
+
 
